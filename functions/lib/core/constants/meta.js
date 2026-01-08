34ab63eed232ad13eba767849f1e64a9fbec85a9
Ivan <ivanochentaytres@gmail.com>
2026-01-08 13:16:40 -0600

    Intento 3 de conectar face

--- functions/lib/core/constants/meta.js
+++ functions/lib/core/constants/meta.js
@@ -4,7 +4,7 @@ exports.META_CONFIG = void 0;
 exports.META_CONFIG = {
     PIXEL_ID: '25721482294159393',
     ACCESS_TOKEN: 'EAAMPefbZC6fwBQS1EHbrRtdkZBtnEZCAFv0ZBCtX1YhKA4ttRKUgUm6EeLwbQwwswFUZAwShkaRu43gl7z29fwrp47nsXLs3NTA5IDqlmVFuf1fcZBx9QfnSEE4OOG4fcL2FumVuDRAotDe9fVU5s6gbkTZAQaMCHRHu7XkJd4hiBbpyvKKC6wbxosK1Y7HxAZDZD',
-    API_VERSION: 'v18.0',
+    API_VERSION: 'v19.0',
     TEST_EVENT_CODE: '' // Can be populated if user provides it later
 };
 //# sourceMappingURL=meta.js.map
