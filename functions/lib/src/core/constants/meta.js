9cd14a8ddb0eabc97cabf1843757c83d48cf14c3
Ivan <ivanochentaytres@gmail.com>
2026-01-27 11:05:49 -0600

    Lista Auditoria, modo prueba off

--- functions/lib/src/core/constants/meta.js
+++ functions/lib/src/core/constants/meta.js
@@ -5,6 +5,6 @@ exports.META_CONFIG = {
     PIXEL_ID: '25721482294159393',
     ACCESS_TOKEN: 'EAAMPefbZC6fwBQS1EHbrRtdkZBtnEZCAFv0ZBCtX1YhKA4ttRKUgUm6EeLwbQwwswFUZAwShkaRu43gl7z29fwrp47nsXLs3NTA5IDqlmVFuf1fcZBx9QfnSEE4OOG4fcL2FumVuDRAotDe9fVU5s6gbkTZAQaMCHRHu7XkJd4hiBbpyvKKC6wbxosK1Y7HxAZDZD',
     API_VERSION: 'v19.0',
-    TEST_EVENT_CODE: 'TEST87487' // ⚠️ DEJAR VACÍO PARA PRODUCCIÓN. Si tiene valor..., los eventos van a "Test Events" ...
+    TEST_EVENT_CODE: '' // ⚠️ DEJAR VACÍO PARA PRODUCCIÓN. Si tiene valor..., los eventos van a "Test Events" ...
 };
 //# sourceMappingURL=meta.js.map
